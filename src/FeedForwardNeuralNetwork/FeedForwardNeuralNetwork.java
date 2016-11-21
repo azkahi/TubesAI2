b98be589ae0f871d1ecc48643c29ee9dfbdaa4b2
zhorifiandi <arizho16@gmail.com>
2016-11-21 07:05:52 +0700

    hehe

--- src/FeedForwardNeuralNetwork/FeedForwardNeuralNetwork.java
+++ src/FeedForwardNeuralNetwork/FeedForwardNeuralNetwork.java
@@ -49,7 +49,7 @@ public class FeedForwardNeuralNetwork extends AbstractClassifier
         int j = 1;
         //j itu buat ngatur banyaknya iterasi training
         //makin banyak makin lama tapi makin akurat
-        while (FFNN.getSumError() != 0 && j <= 100000){
+        while (FFNN.getSumError() != 0 && j <= 1000000){
             System.out.println("\n\n\nIterasi ke - "+j);
             for (int i = 0; i<instances.size(); i++){
                 error = 0;
